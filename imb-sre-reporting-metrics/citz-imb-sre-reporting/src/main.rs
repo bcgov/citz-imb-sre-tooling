32775ea1cc02375749f457030fddab5f2c8eba40
Adam Kroon <adam.kroon@gov.bc.ca>
2025-05-22 12:14:12 -0700

    changing local ip address

--- imb-sre-reporting-metrics/citz-imb-sre-reporting/src/main.rs
+++ imb-sre-reporting-metrics/citz-imb-sre-reporting/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpServer, Responder, HttpResponse};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize)]
+struct HealthResponse {
+    status: String
+}
+
+#[get("/health")]
+async fn health_check() -> impl Responder {
+    web::Json(HealthResponse {
+        status: "OK".into(),
+    })
+}
+
+#[derive(Deserialize)]
+struct GreetRequest {
+    name: String,
+}
+
+#[post("/greet")]
+async fn greet_user(info: web::Json<GreetRequest>) -> impl Responder {
+    HttpResponse::Ok().json(format!("Hello, {}!", info.name))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server is live at http://0.0.0.0:8080");
+    HttpServer::new(|| {
+        App::new()
+            .service(health_check)
+            .service(greet_user)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
